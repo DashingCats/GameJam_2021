a746019c5b73b52385fe5b14aefd933c7c4cfa01
Taylor <taylor.horsman@ontariotechu.net>
2021-01-29 19:16:40 -0500

    Item Locks In Place
    Becomes unmovable after locks

---
+++ GameJam2021/Assets/Scripts/item1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item1 : MonoBehaviour
+{
+    public GameObject red;
+    Collider2D m_collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if((red.transform.position.x <=-1) && (red.transform.position.y >= 2))
+        {
+            red.transform.position = new Vector3(-1, 3, 1);
+            m_collider.enabled = false;
+            
+        }
+        
+    }
+}
