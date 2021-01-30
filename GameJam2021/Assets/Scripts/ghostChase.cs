3aadf5673fa2bdd9dac177702828e8809a1d7f72
Cydney <cydneywade.drennan@ontariotechu.net>
2021-01-29 22:52:07 -0500

    UI and ghost
    Added Ghost Chase; Added Bool so when the item is at the place, it locks; Added text for giving the player hints when they touch the object

---
+++ GameJam2021/Assets/Scripts/ghostChase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghostChase : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject ghost;
+    public float speedOfGhost = 4;
+    private int maxDist = 10;
+    private int minDist = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        if (Vector2.Distance(transform.position, player.transform.position) >= minDist) 
+        {
+            transform.position += transform.forward * speedOfGhost * Time.deltaTime;
+        
+        }
+        if (Vector2.Distance(transform.position, player.transform.position) <= maxDist) 
+        {
+            
+            transform.position += transform.forward * speedOfGhost * Time.deltaTime;
+        }
+    }
+}
--- GameJam2021/Assets/Scripts/item1.cs
+++ GameJam2021/Assets/Scripts/item1.cs
@@ -1,28 +1,91 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class item1 : MonoBehaviour
 {
     public GameObject red;
+    public GameObject stand;
+    public Text mainText;
+
+    Rigidbody2D m_rigi;
     Collider2D m_collider;
+
+    private bool isSealed = false;
 
     // Start is called before the first frame update
     void Start()
     {
         m_collider = GetComponent<Collider2D>();
+        m_rigi = GetComponent<Rigidbody2D>();
+        mainText.text = "";
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            if (isSealed == false)
+            {
+                mainText.text = "Lovely is not?";
+                StartCoroutine(Countdown());
+
+            }
+            else 
+            {
+                mainText.text = "True beauty ,no?";
+                StartCoroutine(Countdown());
+            }
+            
+ 
+        }
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D colTrig)
+    {
+
+        if (colTrig.gameObject.name == "Stand") 
+        {
+            Debug.Log("It has been placed");
+            red.transform.position = stand.transform.position;
+            
+            mainText.text = "It has been sealed";
+            StartCoroutine(Countdown());
+            isSealed = true;
+            m_rigi.isKinematic = true;
+
+        }
+
+    }
+
+    //Begins Countdown for text to disappear
+   private IEnumerator Countdown() 
+    {
+        float counter = 5;
+        
+        while (counter > 0)
+        {
+
+            yield return new WaitForSeconds (1);
+            counter--;
+        }
+        mainText.text = "  ";
+
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        /*
         if((red.transform.position.x <=-1) && (red.transform.position.y >= 2))
         {
             red.transform.position = new Vector3(-1, 3, 1);
             m_collider.enabled = false;
             
         }
-        
+        */
+
     }
 }
